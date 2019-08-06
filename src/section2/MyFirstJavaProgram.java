5c9456b9349d5bed19093421d44a18c9c31fbb78
League Student <league@iMac-11.attlocal.net>
2019-08-06 14:24:30 -0700

    day 2
    day2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,18 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot hamoshe = new Robot();
+hamoshe.setPenColor(100,200,210);
+hamoshe.setSpeed(100);
+	hamoshe.penDown();
 	
-		
+	
+	
+	for(int i=0; i<4; i++) {
+		hamoshe.move(200);
+		hamoshe.turn(90);
+	}
+
 		
 	}
 }
