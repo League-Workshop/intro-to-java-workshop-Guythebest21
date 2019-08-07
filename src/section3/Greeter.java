b411744981c805c2ac3d21f99e44864d2c4e9cf3
League Student <league@iMac-11.attlocal.net>
2019-08-07 14:21:13 -0700

    day 3
    day 3

---
+++ src/section3/Greeter.java
@@ -0,0 +1,16 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+
+	
+	public static void main(String[] args) {
+		
+		
+String name = JOptionPane.showInputDialog("Hamoshe");
+JOptionPane.showMessageDialog(null,"Hello" + name);
+		
+		
+}
+}
--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,14 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+	String answer  = JOptionPane.showInputDialog("do you know how to code");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if(answer.equalsIgnoreCase("yes")) {
 		// 3. Otherwise, wish them good luck washing dishes.
 
+			
+		}else {
+			JOptionPane.showMessageDialog(null, "good luck washing dishes ");
 	}
 }
-
+}
